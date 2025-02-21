20b0eab2b80914d5893374bbc79335b8a6535efa
pejman <pejman.hkh@gmail.com>
2025-02-21 22:11:45 +0330

    Main Menu: Add new component for main menu to use different places

---
+++ src/components/left/main/MainMenu.tsx
@@ -0,0 +1,109 @@
+import React, { FC, memo, StateHookSetter, useMemo } from "../../../lib/teact/teact";
+import { APP_NAME, DEBUG, IS_BETA } from "../../../config";
+import { getActions } from "../../../global";
+import useAppLayout from "../../../hooks/useAppLayout";
+import useFlag from "../../../hooks/useFlag";
+import useLastCallback from "../../../hooks/useLastCallback";
+import useOldLang from "../../../hooks/useOldLang";
+import { useFullscreenStatus } from "../../../hooks/window/useFullscreen";
+import { LeftColumnContent } from "../../../types";
+import buildClassName from "../../../util/buildClassName";
+import { IS_ELECTRON, IS_MAC_OS } from "../../../util/windowEnvironment";
+import Button from "../../ui/Button";
+import DropdownMenu from "../../ui/DropdownMenu";
+import LeftSideMenuItems from "./LeftSideMenuItems";
+import useLeftHeaderButtonRtlForumTransition from "./hooks/useLeftHeaderButtonRtlForumTransition";
+
+type OwnProps = {
+    content: LeftColumnContent,
+    setContent: StateHookSetter<LeftColumnContent>,
+    onReset: (forceReturnToChatList?: true | Event) => void,
+    shouldSkipTransition?: boolean | undefined,
+    shouldHideSearch?: boolean
+    className?: string,
+};
+
+type StateProps = {
+};
+
+const MainMenu: FC<OwnProps & StateProps> = ({ content, setContent, onReset, shouldSkipTransition, shouldHideSearch, className }) => {
+
+    const oldLang = useOldLang();
+    const [isBotMenuOpen, markBotMenuOpen, unmarkBotMenuOpen] = useFlag();
+
+    const onSelectSettings = useLastCallback(() => {
+        setContent(LeftColumnContent.Settings);
+    });
+
+    const onSelectContacts = useLastCallback(() => {
+        setContent(LeftColumnContent.Contacts);
+    });
+
+    const { closeForumPanel } = getActions();
+
+    const onSelectArchived = useLastCallback(() => {
+        setContent(LeftColumnContent.Archived);
+        closeForumPanel();
+    });
+
+    const isFullscreen = useFullscreenStatus();
+    const versionString = IS_BETA ? `${APP_VERSION} Beta (${APP_REVISION})` : (DEBUG ? APP_REVISION : APP_VERSION);
+
+    const { isMobile } = useAppLayout();
+
+    const hasMenu = content === LeftColumnContent.ChatList;
+
+
+    const MainButton: FC<{ onTrigger: () => void; isOpen?: boolean }> = useMemo(() => {
+        return ({ onTrigger, isOpen }) => (
+            <Button
+                round
+                ripple={hasMenu && !isMobile}
+                size="smaller"
+                color="translucent"
+                className={isOpen ? 'active' : ''}
+                // eslint-disable-next-line react/jsx-no-bind
+                onClick={hasMenu ? onTrigger : () => onReset()}
+                ariaLabel={hasMenu ? oldLang('AccDescrOpenMenu2') : 'Return to chat list'}
+            >
+                <div className={buildClassName(
+                    'animated-menu-icon',
+                    !hasMenu && 'state-back',
+                    shouldSkipTransition && 'no-animation'
+                )}
+                />
+            </Button>
+        );
+    }, [hasMenu, isMobile, oldLang, onReset, shouldSkipTransition]);
+
+    const {
+        shouldDisableDropdownMenuTransitionRef,
+        handleDropdownMenuTransitionEnd,
+    } = useLeftHeaderButtonRtlForumTransition(shouldHideSearch);
+
+    return <DropdownMenu
+        trigger={MainButton}
+        footer={`${APP_NAME} ${versionString}`}
+        className={buildClassName(
+            'main-menu',
+            oldLang.isRtl && 'rtl',
+            shouldHideSearch && oldLang.isRtl && 'right-aligned',
+            shouldDisableDropdownMenuTransitionRef.current && oldLang.isRtl && 'disable-transition',
+            className
+        )}
+        forceOpen={isBotMenuOpen}
+        positionX={shouldHideSearch && oldLang.isRtl ? 'right' : 'left'}
+        transformOriginX={IS_ELECTRON && IS_MAC_OS && !isFullscreen ? 90 : undefined}
+        onTransitionEnd={oldLang.isRtl ? handleDropdownMenuTransitionEnd : undefined}
+    >
+        <LeftSideMenuItems
+            onSelectArchived={onSelectArchived}
+            onSelectContacts={onSelectContacts}
+            onSelectSettings={onSelectSettings}
+            onBotMenuOpened={markBotMenuOpen}
+            onBotMenuClosed={unmarkBotMenuOpen}
+        />
+    </DropdownMenu>
+}
+
+export default memo(MainMenu)
