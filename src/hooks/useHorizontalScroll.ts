f0eb16f83f8d2be001ce7afadb44b3a56826c8ac
pejman hkh <pejman.hkh@gmail.com>
2025-04-27 20:27:47 +0330

    Horizontal Scroll: Add scrollContainerRef

--- src/hooks/useHorizontalScroll.ts
+++ src/hooks/useHorizontalScroll.ts
@@ -4,6 +4,8 @@ const useHorizontalScroll = (
   containerRef: React.RefObject<HTMLDivElement>,
   isDisabled?: boolean,
   shouldPreventDefault = false,
+  callback?: (e: WheelEvent) => void,
+  scrollContainerRef?: React.RefObject<HTMLDivElement>,
 ) => {
   useEffect(() => {
     if (isDisabled) {
@@ -13,19 +15,27 @@ const useHorizontalScroll = (
     const container = containerRef.current!;
 
     function handleScroll(e: WheelEvent) {
+      let scrollContainer = container;
+      if (scrollContainerRef?.current) {
+        scrollContainer = scrollContainerRef?.current;
+      }
       // Ignore horizontal scroll and let it work natively (e.g. on touchpad)
       if (!e.deltaX) {
-        container.scrollLeft += e.deltaY / 4;
+        scrollContainer.scrollLeft += e.deltaY / 4;
         if (shouldPreventDefault) e.preventDefault();
+      }
+
+      if (callback) {
+        callback(e);
       }
     }
 
-    container.addEventListener('wheel', handleScroll, { passive: !shouldPreventDefault });
+    container?.addEventListener('wheel', handleScroll, { passive: !shouldPreventDefault });
 
     return () => {
-      container.removeEventListener('wheel', handleScroll);
+      container?.removeEventListener('wheel', handleScroll);
     };
-  }, [containerRef, isDisabled, shouldPreventDefault]);
+  }, [containerRef, isDisabled, shouldPreventDefault, callback, scrollContainerRef]);
 };
 
 export default useHorizontalScroll;
